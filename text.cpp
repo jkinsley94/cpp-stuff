6af30db8c8989fe0b069dd09f9fa0babb5c16e4a
Jason Kinsley <123510189+FufferPuffer94@users.noreply.github.com>
2023-05-03 21:37:24 -0600

    made a simple loop file

---
+++ text.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(int argc, char const *argv[])
+{
+    int num = 0;
+    while (num < 10000)
+    {
+        num++;
+    }
+    std:: cout << num << std::endl;
+    system("pause");
+    return 0;
+}
